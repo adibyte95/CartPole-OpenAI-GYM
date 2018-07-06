f542488f779c2951fcd28572d25e0fc625266415
adibyte95 <singhaditya08aug@gmail.com>
2018-07-07 01:53:30 +0530

    changes

--- prog.py
+++ prog.py
@@ -17,18 +17,20 @@ action:
 1 for right
 '''
 checkpoint = ModelCheckpoint('model.h5', monitor='val_loss',verbose=1, save_best_only=True)
-no_of_observations = 200
-min_score = 60
+no_of_observations = 500
+min_score = 100
 
 
 # generate the training data 
 def generate_training_data(no_of_episodes):
     # initize the environment
-    env = gym.make('CartPole-v0')
+    env = gym.make('CartPole-v0').env
     X = []
     y =[]
     prev_X = []
     prev_y= []
+    left = 0
+    right = 0
 
     for i_episode in range(no_of_episodes):
         observation = env.reset()
@@ -53,10 +55,15 @@ def generate_training_data(no_of_episodes):
                 # if the episode is not over
                 X.append(prev_observation)
                 if action ==0:
+                    left = left + 1
                     y.append([1,0])
                 elif action ==1:
+                    right = right +1
                     y.append([0,1])
         env.reset()
+    
+    print('left : ', left)
+    print('right: ',right)
     # converting them into numpy array
     X = np.asarray(prev_X)
     y =np.asarray(prev_y) 
@@ -72,23 +79,23 @@ def generate_training_data(no_of_episodes):
 # defines the model to be trained
 def get_model():
     model = Sequential()
-    model.add(Dense(32, input_dim=4))
+    model.add(Dense(128, input_dim=4))
     model.add(Activation('relu'))
     model.add(Dropout(.2))
 
-    model.add(Dense(64))
+    model.add(Dense(256))
+    model.add(Activation('relu'))
+    model.add(Dropout(.2))
+
+    model.add(Dense(512))
+    model.add(Activation('relu'))
+    model.add(Dropout(.2))
+
+    model.add(Dense(256))
     model.add(Activation('relu'))
     model.add(Dropout(.2))
 
     model.add(Dense(128))
-    model.add(Activation('relu'))
-    model.add(Dropout(.2))
-
-    model.add(Dense(64))
-    model.add(Activation('relu'))
-    model.add(Dropout(.2))
-
-    model.add(Dense(32))
     model.add(Activation('relu'))
     model.add(Dropout(.2))
 
@@ -117,37 +124,34 @@ def train_model(model):
 # testing the model 
 def testing():
     model = load_model('model.h5')
-    env = gym.make('CartPole-v0')
-    env.reset()
+    env = gym.make('CartPole-v0').env
+    observation = env.reset()
     # initial score
     score =0
-    # run the loop until the game is lost
-    prev_observation = []
-    action = 0
 
+    action = 0
     while (True):
         env.render()
-        if score ==0:
+        input('aa')
+        data = np.asarray(observation)
+        data = np.reshape(data, (1,4))
+        output = model.predict(data)
+        '''
+        if output[0][0] >= output[0][1]:
+            action = 1
+        elif output[0][0] < output[0][1]:
             action = 0
-        else:
-            prev_observation = np.asarray(prev_observation)
-            prev_observation = np.reshape(prev_observation, (1,4))
-            output = model.predict(prev_observation)
-            if output[0][0] >= output[0][1]:
-                action = 0
-            elif output[0][0] < output[0][1]:
-                action = 1
-
-        new_observation, reward, done, info = env.step(action)
-        prev_observation = new_observation
-        
+        print(action)
+        '''
+        observation, reward, done, info = env.step(action)
         score = score  + reward 
-        if done == 1:
+        
+        if done:
             print('game over!! your score is :  ',score)
             env.reset()
             break
 
-generate_training_data(100000)
-model = get_model()
-model = train_model(model)
+#generate_training_data(1000)
+#model = get_model()
+#model = train_model(model)
 testing()
---
+++ prog_brute.py
@@ -0,0 +1,52 @@
+# importing the dependencies
+import gym
+import random
+import numpy as np 
+from gym import wrappers
+
+env = gym.make('CartPole-v0')
+bestLength = 0
+episode_length =[]
+best_weights = np.zeros(4)
+
+for i in range(100):
+    if i%10 ==0:
+        print('i: ',i)
+    new_weights = np.random.uniform(-1, 1, 4)
+    length = []
+    for j in range(100):
+        print(j)
+        observation = env.reset()
+        done = False
+        count = 0
+        while not done:
+            count = count +1
+            action = 1 if np.dot(observation,new_weights) >0 else 0
+            observation,reward,done,_ = env.step(action)
+            if done:
+                break
+        length.append(count)
+    avg_length = float(sum(length) / len(length))
+
+    if avg_length >bestLength:
+        bestLength = avg_length
+        best_weights = new_weights 
+    episode_length.append(avg_length)
+    if i%10 ==0:
+        print('best length is: ',bestLength)
+    
+
+print(best_weights)
+
+done=  False
+count = 0
+env = wrappers.Monitor(env,'files',force = True)
+observation = env.reset()
+
+while not done:
+    count = count +1
+    action = 1 if np.dot(observation,new_weights) >0 else 0
+    observation,reward,done,_ = env.step(action)
+    if done:
+        break
+print('with best weights, game lasted ',count , ' moves')
