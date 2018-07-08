21ff0d942307ec45d22ef17848ddf231b7348d1b
ADITYA SINGH <singhaditya08aug@gmail.com>
2018-07-08 21:34:27 +0530

    small changes

--- prog_bruteforce.py
+++ prog_bruteforce.py
@@ -1,21 +1,22 @@
+# make a random vector which gives good result
+
 # importing the dependencies
 import gym
 import random
 import numpy as np 
 from gym import wrappers
 
-env = gym.make('CartPole-v0')
+env = gym.make('CartPole-v1').env
 bestLength = 0
 episode_length =[]
 best_weights = np.zeros(4)
+flag = 0
+max_life = 1000
 
 for i in range(10):
-    if i%10 ==0:
-        print('i: ',i)
     new_weights = np.random.uniform(-1, 1, 4)
     length = []
-    for j in range(100):
-        print(j)
+    for j in range(500):
         observation = env.reset()
         done = False
         count = 0
@@ -25,6 +26,9 @@ for i in range(10):
             observation,reward,done,_ = env.step(action)
             if done:
                 break
+            elif count > max_life:
+                flag =1
+                break
         length.append(count)
     avg_length = float(sum(length) / len(length))
 
@@ -32,20 +36,21 @@ for i in range(10):
         bestLength = avg_length
         best_weights = new_weights 
     episode_length.append(avg_length)
-    if i%10 ==0:
-        print('best length is: ',bestLength)
-    
+    if flag ==1:
+        break
 
 print(best_weights)
 
+
+## testing
+env= wrappers.Monitor(env, 'brute_force_files', force = True)
 done=  False
 count = 0
-env = wrappers.Monitor(env,'files',force = True)
 observation = env.reset()
 
 while not done:
     count = count +1
-    action = 1 if np.dot(observation,new_weights) >0 else 0
+    action = 1 if np.dot(observation,best_weights) >0 else 0
     observation,reward,done,_ = env.step(action)
     if done:
         break
