bf9583abc4e3fc87303639db980c98b08c0447e3
ADITYA SINGH <singhaditya08aug@gmail.com>
2018-07-12 02:28:12 +0530

    minor changes

--- test file/test_prog_rl.py
+++ test file/test_prog_rl.py
@@ -1,5 +1,6 @@
 import random
 import gym
+from gym import wrappers
 import numpy as np
 from collections import deque
 from keras.models import Sequential
@@ -13,6 +14,7 @@ model = load_model("model/cartpole-dqn.h5")
 #model.load_weights("model/cartpole-ddqn.h5")
 
 env = gym.make('CartPole-v0').env
+env= wrappers.Monitor(env, 'reinforcement files', force = True)
 state = env.reset()
 print('state is : ',state)
 done = False
@@ -20,7 +22,8 @@ count = 0
 count_left=0
 count_right = 0
 
-while not done:
+while count < 500:
+    count = count_left + count_right 
     env.render()
     state = np.reshape(state, [1, 4])
     action = model.predict(state)
@@ -34,6 +37,7 @@ while not done:
     next_state,reward,done,info = env.step(action)
     state = next_state
     count = count + 1
+    print('count: ', count)
 print('count left: ',count_left)
 print('count right: ',count_right)
 print(count)
